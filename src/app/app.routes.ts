513413e039742151f24fa36c4ad2074fe147d461
Jony <35571614+Jonyzim@users.noreply.github.com>
2024-06-23 17:38:28 -0300

    teste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import {MedicamentStockComponent} from "./medicament-stock/medicament-stock.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
 import {PatientViewComponent} from "./patient-view/patient-view.component";
+import {LoginComponent} from "./login/login.component";
 
 export const routes: Routes = [
-  { path:  '', component:  PatientViewComponent},
+  { path:  '', component:  LoginComponent},
   { path:  'medicamentos', component:  MedicamentStockComponent},
   { path:  'pacientes', component:  PatientViewComponent},
   {path: '404', component: NotFoundComponent},
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-logins',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+
+}
